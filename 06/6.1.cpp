9f4aace31d366af1224d2dad0bde8179caef3753
jnozsc <jnozsc@gmail.com>
2014-03-10 14:08:31 -0400

    first commit

---
+++ 06/6.1.cpp
@@ -0,0 +1,13 @@
+void quicksort(int[] A, int int pivot_index) {
+    int pivot = A[pivot_index];
+    int smaller = 0, equal = 0, larger = sizeof(A) / sizeof(*A) - 1;
+    while (equal <= larger) {
+        if (A[equal] < pivot) {
+            swap(A[smaller++], A[equal++])
+        } else if (A[equal] == pivot) {
+            equal++;
+        } else {
+            swap(A[equal], A[larger--]);
+        }
+    }
+}
